e5a211ce2bbe40960c8b5aa07a6b1f15260216b6
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-08-28 20:57:57 +0530

    ArthemeticOperations

---
+++ ArthemeticOperators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number1,number2;
+    cout<<"Please enter your numbers:";
+    cin>>number1>>number2;
+    cout<<"Addition is:"<<number1+number2<<endl;
+    cout<<"Subtraction is:"<<number1-number2<<endl;
+    cout<<"Multiplication is:"<<number1*number2<<endl;
+    cout<<"Division is:"<<number1/number2<<endl;
+    cout<<"Modulo is:"<<number1%number2<<endl;
+    return 0;
+}
