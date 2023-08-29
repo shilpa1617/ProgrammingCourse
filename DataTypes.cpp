0acdf4fe85fca69e72beee48c1814c464e7c2563
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-08-29 20:42:13 +0530

    DataTypes Problem

---
+++ DataTypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int number;
+    float number1;
+    double number2;
+    char sh;
+    
+    cin>>number;
+    cin>>number1;
+    cin>>number2;
+    cin>>sh;
+
+    cout<<"The double value is "<<number2<<endl;
+    cout<<"The float value is "<<number1<<endl;
+    cout<<"The int value is "<<number<<endl;
+    cout<<"The char is "<<sh<<endl;
+    return 0;
+}
