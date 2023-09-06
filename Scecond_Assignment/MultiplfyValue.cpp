ea387f490e17c9b754235de5503d77b68b5edd03
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-06 18:32:08 +0530

    multiply value of four accepted numbers

---
+++ Scecond_Assignment/MultiplfyValue.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2,num3,num4,result;
+    cout<<"Enter any four numbers : ";
+    cin>>num1>>num2>>num3>>num4;
+    result=num1*num2*num3*num4<<endl;
+    cout<<result<<endl;
+
+    return 0;
+
+}
