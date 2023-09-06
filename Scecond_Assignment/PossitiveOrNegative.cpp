ad076ceb03b3a1a07b4bc9d0a0c8e3d75e5f9659
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-06 19:12:31 +0530

    Number is Possitive or Negative

---
+++ Scecond_Assignment/PossitiveOrNegative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"Enter a Number:";
+    cin>>number;
+    if (number>0)
+    {
+        cout<<"The entered number is Possitive"<<endl;
+    }
+    else{
+        cout<<"The entered number is Negative"<<endl;
+    }
+    return 0;
+}
