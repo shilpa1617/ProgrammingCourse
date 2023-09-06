bb9f4b4c839fb6ee150e47db94f1e3c44add253e
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-06 18:41:02 +0530

    number is ASCII value of ‘A’ or not

---
+++ Scecond_Assignment/ASCIIValueOfA.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A;
+    cin>>A;
+    if (A==65)
+    {
+        cout<<"Yes,It is"<<endl;
+    }
+    else{
+        cout<<"No, It is not"<<endl;
+    }
+    return 0;
+}
