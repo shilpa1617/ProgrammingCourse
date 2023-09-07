6ead1bd1321a4ea061301cba165e6c267c413e15
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-07 21:07:44 +0530

    person1Age

--- SessionHandsOn/TernaryOperator.cpp
+++ SessionHandsOn/TernaryOperator.cpp
@@ -5,7 +5,8 @@ int main(){
     int person1Age,person2Age;
     cout<<"Enter two person ages:";
     cin>>person1Age>>person2Age;
-    int result=(person1Age>person2Age)?person1Age:person2Age;
+    int result=(person1Age>person2Age) ? person1Age:person2Age;
     cout<<result<<endl;
+    
     return 0;
 }
