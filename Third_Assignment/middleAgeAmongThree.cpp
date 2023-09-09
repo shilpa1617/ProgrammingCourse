4249d22727f9457abc1362b587083f66c5a68f53
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-09 23:54:09 +0530

    Middle age among three

---
+++ Third_Assignment/middleAgeAmongThree.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int person1Age,person2Age,person3Age;
+    cin>>person1Age>>person2Age>>person3Age;
+
+    if ((person1Age >= person2Age && person1Age <= person3Age) || (person1Age <= person2Age && person1Age >= person3Age)) {
+        std::cout << "Person 1 is the middle person " <<person1Age << std::endl;
+    }
+    if ((person2Age >= person1Age && person2Age <= person3Age) || (person2Age <= person1Age && person2Age >= person3Age)) {
+        cout << "Person 2 is the middle person " << person2Age <<endl;
+    }
+    else {
+        cout << "Person 3 is the middle person" <<person3Age <<endl;
+    }
+
+    return 0;
+}
