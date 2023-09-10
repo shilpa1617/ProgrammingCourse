a1f7e0dcdebb5e5f8e6475850cadb1499402bdd5
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-10 15:13:11 +0530

    number is possitive or negative

---
+++ Third_Assignment/possitiveOrNegative.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cin>>number;
+    if(number>0){
+        cout<<number<<" is possitive number "<<endl;
+    }
+    else if(number<0){
+         cout<<number<<" is negative number "<<endl;
+    }
+    else{
+        cout<<"Zero"<<endl;
+    }
+
+    return 0;
+}
