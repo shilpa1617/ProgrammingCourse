fc8f7bf374e1944ca0ce1073b9e0c45b74328d81
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-11 20:51:08 +0530

    operators

--- Third_Assignment/operators.cpp
+++ Third_Assignment/operators.cpp
@@ -1,28 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int number1,number2,number3;
+int main()
+{
+    int number1, number2, number3;
     char op;
-    cin>>number1>>number2>>number3;
-    cin>>op;
-    if(op=='+'){
-        cout<<"The addition is:"<<number1+number2+number3<<endl;
+    cin >> number1 >> number2 >> number3;
+    cin >> op;
+    if (op == '+')
+    {
+        cout << "The addition is:" << number1 + number2 + number3 << endl;
     }
-    else if(op=='-'){
-        cout<<"The subtraction is:"<<number1-number2-number3<<endl;
+    else if (op == '-')
+    {
+        cout << "The subtraction is:" << number1 - number2 - number3 << endl;
     }
-    else if(op=='*'){
-        cout<<"The multiplication is:"<<number1*number2*number3<<endl;
+    else if (op == '*')
+    {
+        cout << "The multiplication is:" << number1 * number2 * number3 << endl;
     }
-    else if(op=='/'){
-        cout<<"The division is:"<<(number1+number2)/number3<<endl;
+    else if (op == '/')
+    {
+        cout << "The division is:" << (number1 + number2) / number3 << endl;
     }
-    else{
-        cout<<"Please enter validate character"<<endl;
+    else
+    {
+        cout << "Please enter validate character" << endl;
     }
-    
-
-
     return 0;
 }
