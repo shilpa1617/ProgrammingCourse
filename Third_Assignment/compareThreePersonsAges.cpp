9a40117f359b16904f5c263f118129fa70f12cc7
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-11 12:59:43 +0530

    comparing three person ages

--- Third_Assignment/compareThreePersonsAges.cpp
+++ Third_Assignment/compareThreePersonsAges.cpp
@@ -1,19 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int ravulaAge,ashwiniAge,priyankaAge;
-    cout<<"Enter Ravula,Ashwini and Priyanka ages:";
-    cin>>ravulaAge>>ashwiniAge>>priyankaAge;
-    if((ravulaAge<ashwiniAge)&&(ravulaAge<priyankaAge)){
-        cout<<"Ravula is the younger one"<<endl;
-    }
-    else if ((ashwiniAge<ravulaAge)&&(ashwiniAge<priyankaAge))
+int main()
+{
+    int ravulaAge, ashwiniAge, priyankaAge;
+    cout << "Enter Ravula,Ashwini and Priyanka ages:";
+    cin >> ravulaAge >> ashwiniAge >> priyankaAge;
+    if ((ravulaAge < ashwiniAge) && (ravulaAge < priyankaAge))
     {
-        cout<<"Ashwini is the younger one"<<endl;
+        cout << "Ravula is the younger one" << endl;
     }
-    else{
-        cout<<"Priyanka is the younger one"<<endl;
+    else if ((ashwiniAge < ravulaAge) && (ashwiniAge < priyankaAge))
+    {
+        cout << "Ashwini is the younger one" << endl;
+    }
+    else
+    {
+        cout << "Priyanka is the younger one" << endl;
     }
     return 0;
 }
