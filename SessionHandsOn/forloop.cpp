0d00cad3eeee15a4e0faf0716eacb8840aebb8f1
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-13 21:12:30 +0530

    print a-z

---
+++ SessionHandsOn/forloop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char alphabet;
+    for(alphabet='a';alphabet<='z';alphabet++){
+        cout<<alphabet<<endl;
+    }
+
+
+    return 0;
+}
