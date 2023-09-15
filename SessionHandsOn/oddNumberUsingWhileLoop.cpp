86d7d11388913f1ecf2e0b8dc89732e3f41b8a3a
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-15 21:11:06 +0530

    Odd number using while loop

---
+++ SessionHandsOn/oddNumberUsingWhileLoop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=1,number;
+    cin>>number;
+    while(i<=number){
+         cout<<"Odd number is :"<<i<<endl;
+        i+=2; 
+    }
+    return 0;
+}
