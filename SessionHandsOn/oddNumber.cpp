93a72faa0303f3e3ff58d39a78321f2e7059e2b8
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-15 20:04:21 +0530

    modified file

--- SessionHandsOn/oddNumber.cpp
+++ SessionHandsOn/oddNumber.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>n;
     for(i=0;i<=n;i++){
         if(i%2!=0){
-            cout<<"The even number is:"<<i<<endl;
+            cout<<"The odd number is:"<<i<<endl;
         }
     }
     
