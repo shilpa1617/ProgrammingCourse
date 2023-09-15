eb77d127631d5735a2e05ab7f369cbcfd6ce70d4
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-15 20:57:39 +0530

    even number using while loop

---
+++ SessionHandsOn/whileLoop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=2,number;
+    cin>>number;
+    while(i<=number){
+         cout<<"Even number is :"<<i<<endl;
+        i=i+2; 
+    }
+    return 0;
+}
