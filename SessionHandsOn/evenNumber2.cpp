76672341480d85c21217888bb91cf70e66641d53
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-15 20:03:42 +0530

    even number without if condition

---
+++ SessionHandsOn/evenNumber2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,n;
+    cin>>n;
+    for(i=0;i<=n;i+=2){
+        
+            cout<<"The even number is:"<<i<<endl;
+
+    }
+    return 0;
+}
