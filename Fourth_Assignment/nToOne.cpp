fe6fecec190496d914b563cf6d5666c62b5a72ef
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-16 20:01:21 +0530

    Printing numbers form N to 1

---
+++ Fourth_Assignment/nToOne.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+    for (int i=number; i>0; i--)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
