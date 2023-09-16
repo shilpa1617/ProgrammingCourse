bfce46081c300bc3a4a82994a161d8d140e14a9c
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-16 20:39:48 +0530

    multiplication table

--- Fourth_Assignment/multiplication.cpp
+++ Fourth_Assignment/multiplication.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int i,number;
     cin>>number;
-    for(i=0;i<=10;i++){
+    for(i=1;i<=10;i++){
         int result=number*i;
             cout<<number<<" * "<<i<<" = "<<result<<endl;
             if(result%2==0){
