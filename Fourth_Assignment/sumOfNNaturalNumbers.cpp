273b1aee7c7d09c2af5ca9e16152aa7c17cb1fe3
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-18 20:19:50 +0530

    sum of natural numbers

--- Fourth_Assignment/sumOfNNaturalNumbers.cpp
+++ Fourth_Assignment/sumOfNNaturalNumbers.cpp
@@ -6,9 +6,9 @@ int main(){
     cin>>number;
     
     for(i=1;i<=number;i++){
-        sum=sum+i;
-        result=2*number+sum;
+        sum=sum+i; 
     }
+    result=2*number+sum;
     cout<<"Sum of 1 to "<<number<<":"<<sum<<endl;
     cout<<result<<endl;
     return 0;
