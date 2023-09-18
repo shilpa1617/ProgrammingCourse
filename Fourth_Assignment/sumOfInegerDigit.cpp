3d78fed99dcd936618f30e4f755ed9d7eaf08a41
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-18 19:06:49 +0530

    Sum of interger numbers

---
+++ Fourth_Assignment/sumOfInegerDigit.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number, sum = 0;
+    cin >> number;
+    while(number!=0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    cout << sum << endl;
+    return 0;
+}
