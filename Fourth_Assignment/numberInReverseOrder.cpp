f67a9b0ad3aef0cdf9122d742fd1260148ad4b15
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-19 21:17:52 +0530

    number in a reverse order

--- Fourth_Assignment/numberInReverseOrder.cpp
+++ Fourth_Assignment/numberInReverseOrder.cpp
@@ -8,14 +8,12 @@ int main()
 
     while (number != 0)
     {
-        int value=number%10;
-        rem = (rem * 10) + value;
-        cout<<value<<" ";
+        rem = (rem * 10) + number%10;
          //cout << rem << endl;
         number = number / 10;
         // cout<<number<<endl;
     }
-    // cout <<rem<<endl;
+    cout <<rem<<endl;
 
     return 0;
 }
