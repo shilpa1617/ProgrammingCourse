10d1f990e6feca058ab2d7cd77d975cf0e561932
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-22 20:50:19 +0530

    print 10-20 using dowhileLoop

---
+++ SessionHandsOn/doWhileLoop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number=10;
+    do
+    {
+     cout<<number<<endl; 
+     number++;
+    } while (number<=20); 
+    
+return 0;
+}
+// do{
+    //block statements
+// }while(condition);
