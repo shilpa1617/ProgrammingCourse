0c7b6a3d6380c420a74e7759696c8ad6a54472bb
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-09-23 22:38:07 +0530

    update

--- Fifth_Assigment/sumOfEvenOddNumber.cpp
+++ Fifth_Assigment/sumOfEvenOddNumber.cpp
@@ -1,25 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int sum=0,count=0,sum1=0,count1=0;
-    for(int number=1;number<=100;number++){
-        if(number%2==0){
-            sum+=number;
+int main()
+{
+    int sum = 0, count = 0, sum1 = 0, count1 = 0;
+    for (int number = 1; number <= 100; number++)
+    {
+        if (number % 2 == 0)
+        {
+            sum += number;
             count++;
         }
-    }
-    cout<<sum<<endl;
-    cout<<count<<endl;
-    cout<<"The average of even numbers 1-100: "<<(sum/count)<<endl;
-    for(int number=1;number<=100;number++){
-        if(number%2!=0){
-            sum1+=number;
+        else
+        {
+            sum1 += number;
             count1++;
         }
     }
-    cout<<sum1<<endl;
-    cout<<count1<<endl;
-    cout<<"The average of odd numbers 1-100: "<<(sum1/count1)<<endl;
+    cout << sum << endl;
+    cout << count << endl;
+    cout << "The average of even numbers 1-100: " << (sum / count) << endl;
+
+    cout << sum1 << endl;
+    cout << count1 << endl;
+    cout << "The average of odd numbers 1-100: " << (sum1 / count1) << endl;
     return 0;
 }
