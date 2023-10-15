5b7ac553c670ed2858be008175ef8e1d103c07c6
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-10-15 17:56:17 +0530

    sum of numbers in an vector

---
+++ Eighth_Assignment/sumOfNumberInVector.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N,num;
+    cin>>N;
+    vector<int> numbers;
+    for(int i=0;i<N;i++){
+        cin>>num;
+        numbers.push_back(num);
+    }
+    int sum=0;
+    for(int i=0;i<N;i++){
+        sum=sum+numbers[i];
+    }
+    cout<<sum<<endl;
+    return 0;
+}
