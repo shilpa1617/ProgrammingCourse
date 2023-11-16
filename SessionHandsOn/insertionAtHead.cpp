bd6c46d219a11a42f4312c33f331006bfe6b4039
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-11-16 23:28:14 +0530

    insertion at head

---
+++ SessionHandsOn/insertionAtHead.cpp
@@ -0,0 +1,45 @@
+// insertionAtTail
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *insertionAtHead(struct Node *head)
+{
+    struct Node *newNode = new Node();
+    int number;
+    cin >> number;
+    newNode->data = number;
+    if (head == NULL)
+    {
+        newNode->next = NULL;
+    }
+    else
+    {
+        newNode->next = head;
+    }
+    head = newNode;
+    return head;
+}
+struct Node *display(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    struct Node *head = NULL;
+    head = insertionAtHead(head);
+    head = insertionAtHead(head);
+    head = insertionAtHead(head);
+    display(head);
+    // cout << "head value :" << head->data << endl;
+}
