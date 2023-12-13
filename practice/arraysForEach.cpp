c6413aa811e086cbe939537d455f5a9e1b18b643
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-12-13 21:58:39 +0530

    foreach loop simple example

---
+++ practice/arraysForEach.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int numbers[5]={10,20,30,40,50};
+    // for(int i=0;i<5;i++){
+    //     cout<<numbers[i]<<endl;
+    // }
+    for(int num : numbers){
+        cout<<num<<endl;
+    }
+    return 0;
+}
