a52dc52d92b4e5330ffd30970910867bcbdecc9e
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-12-15 21:43:31 +0530

    sum of first  and last two numbers

--- Seventh_Assignment/firsLastTwoNumbersSum.cpp
+++ Seventh_Assignment/firsLastTwoNumbersSum.cpp
@@ -1,20 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int N;
     cin >> N;
     int numbers[N];
 
-    if(N>4){
-    for (int i = 0; i < N; i++)
+    if (N > 4)
     {
-        cin >> numbers[i];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> numbers[i];
+        }
+        cout << numbers[0] + numbers[1] + numbers[N - 1] + numbers[N - 2] << endl;
     }
-    cout<<numbers[0]+numbers[1]+numbers[N-1]+numbers[N-2]<<endl;
-    }
-    else{
-        cout<<"Array Size is less than four "<<endl;
+    else
+    {
+        cout << "Array Size is less than four " << endl;
     }
     return 0;
 }
