ed56927ec85f3f0adabf9b5add18ab4158016e33
prabhajanagama <prabhakar.janagama@thoughtworks.com>
2023-12-19 20:55:36 +0530

    basic  pointer programs

---
+++ practice/pointers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number=12;
+    int *ptr=&number;
+
+    cout<<number<<" "<<"address :"<<ptr<<" Number "<<*ptr<<endl;
+    *ptr=17;
+    cout<<number<<" "<<"address :"<<ptr<<" Number "<<*ptr<<endl;
+
+    void *pointer;
+    int variable=10;
+    pointer=&variable; 
+
+    cout<<pointer<<endl;
+    cout<<*pointer<<endl;
+
+    return 0;
+}
